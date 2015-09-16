e7156b135a20ab8b867606dd390c75d22061f651
Thomas Worthington <thomas@technouveau.co.uk>
2015-09-16 08:26:59 +0100

    Added patrols and strongholds

--- encounter.pl
+++ encounter.pl
@@ -4,6 +4,40 @@ $terrain= shift or die("I need a terrain to generate an encounter in\n");
 
 die("The terrain value has to be a directory\n") if(! -d $terrain);
 
+$dmgflag=shift || 0;
+
+
+if($dmgflag && $terrain=~/land/)
+{
+    if($terrain=~/civilized/)
+    {
+        if(int(rand(20)+1)<6)
+        {
+            print "Patrol\n";
+            exit;
+        }
+    }
+    elsif($terrain=~/wilderness/)
+    {
+        if(int(rand(20)+1)<2)
+        {
+            print "Stronghold:\t";
+            print pickone(100,(10,'Small shell keep (small)',
+                               25,'Tower (small)',
+                               35,'Moat house or friary (small)',
+                               45,'Large shell keep (medium)',
+                               65,'Small walled castle with keep (medium)',
+                               80,'Medium walled castle with keep (medium)',
+                               88,'Concentric castle (large)',
+                               95,'Large walled castle with keep (large)',
+                               100,'Fortress complex (large)')
+                );
+            print "\n";
+            exit;
+        }
+    }
+}
+
 $n=int(rand(100))+1;
 
 if($n<66)
@@ -29,3 +63,18 @@ die("Can't find $filename\n") unless( -f $filename);
 
 print `sort -R "$filename"|head -1`;
     
+sub pickone
+{
+    my $sides=shift;
+    my %results=@_;
+
+    my $roll=int(rand($sides)+1);
+
+    foreach( sort { $a <=> $b } keys %results )
+    {
+        if($_>=$roll)
+        {
+            return $results{$_};
+        }
+    }
+}
